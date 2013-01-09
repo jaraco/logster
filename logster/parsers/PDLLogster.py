55760fe6c442aeac6eb32d7c6c1d1b6e413df1f6
Alfredo Deza <alfredodeza@gmail.com>
2013-01-09 14:07:34 -0500

    minor cleanup of docstring samples

--- logster/parsers/PDLLogster.py
+++ logster/parsers/PDLLogster.py
@@ -7,13 +7,11 @@ class PDLLogster(LogsterParser):
     def __init__(self):
         self.data = {'servers':{}, 'callers':{}}
 
-
     def parse_line(self, line):
         """
         Lines come in a format similar to:
-        Jan  8 20:56:56 pdl01 logger: @4000000050ec8822393be4c4 2013-01-08 20:56:56,959 PROXY proxy         INFO     [sampling 3a9141d1-3eb2-4b4c-9e2e-2153d086fb1c] CALL get_answers(
-        Ene  1 11:11:11 srv01 logger: @0000000000sdf789sad9f876 2012-01-01
-        Month Day HH:MM:SS server logger: @some_hash YYYY-MM-DD HH:MM:SS,SSS PROXY proxy LEVEL [caller hash-hash] CALL some_call(\d+, '\w+')
+        Month Day HH:MM:SS server logger: @some_hash YYYY-MM-DD HH:MM:SS,SSS\
+        PROXY proxy LEVEL [caller hash-hash] CALL some_call(\d+, '\w+')
         """
         pattern = r'.*\s+INFO\s+\[(?P<server_id>\w+)\s+(?P<caller_hash>[a-zA-Z0-9_-]+)\]\s+CALL\s+(?P<caller_name>\w+)'
         regex = re.compile(pattern)
